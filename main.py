02b01ddfdcff2b944565743e053fb951659996fc
luna <luna.s.jo@polarisoffice.com>
2024-06-17 16:34:02 +0900

    chat gpt 연동 완료

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import tensorflow as tf
-from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
 import numpy as np
-import json
 import openai
 import os
 import requests
 from dotenv import load_dotenv
-from PIL import Image
 
 load_dotenv()
-openai.api_key = os.getenv('API_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # 이미지를 전처리하는 함수
 def preprocess_image(img_path):
@@ -23,26 +21,32 @@ def preprocess_image(img_path):
 
 # 품종을 예측하는 함수
 def predict_breed(img_path):
-    model = VGG16(weights='imagenet', include_top=True)
+    model = MobileNetV2(weights='imagenet')
     img = preprocess_image(img_path)
     preds = model.predict(img)
-    predicted_breed = np.argmax(preds[0])  # 가장 높은 확률을 가진 클래스의 인덱스
-    return predicted_breed
+    decoded_preds = decode_predictions(preds, top=1)[0][0][1]
+    return decoded_preds
 
-def load_class_index():
-    with open('imagenet_class_index.json') as f:
-        class_index = json.load(f)
-    # 클래스 인덱스를 품종으로 매핑
-    class_index_to_breed = {int(idx): label for idx, (_, label) in class_index.items()}
-    return class_index_to_breed
+# 강아지 이미지 경로
+img_path1 = 'data_set/dog.jpg'
+img_path2 = 'data_set/dog2.jpg'
+
+# 품종 예측 및 출력
+predicted_breed1 = predict_breed(img_path1)
+predicted_breed2 = predict_breed(img_path2)
+
+print("Predicted Breed 1:", predicted_breed1)
+print("Predicted Breed 2:", predicted_breed2)
 
 def generate_image(prompt):
-    response = openai.Image.create(
+    response = openai.images.generate(
+        model="dall-e-3",
         prompt=prompt,
+        size="1024x1024",
+        quality="hd",
         n=1,
-        size="1024x1024"
     )
-    image_url = response['data'][0]['url']
+    image_url = response.data[0].url
     return image_url
 
 def save_image(image_url, file_path):
@@ -55,32 +59,6 @@ def save_image(image_url, file_path):
     else:
         print("Failed to retrieve the image")
 
-# 강아지 이미지 경로
-img_path1 = 'data_set/dog.jpg'
-img_path2 = 'data_set/dog2.jpg'
-
-# 품종 예측 및 출력
-predicted_breed1 = predict_breed(img_path1)
-predicted_breed2 = predict_breed(img_path2)
-class_index_to_breed = load_class_index()
-
-dog1 = ''
-dog2 = ''
-
-# 예측된 품종 인덱스를 이용하여 품종 출력 (첫번째 강아지)
-if predicted_breed1 in class_index_to_breed:
-    dog1 = class_index_to_breed[predicted_breed1]
-    print("Predicted Breed:", dog1)
-else:
-    print("Invalid breed index:", predicted_breed1)
-
-# 예측된 품종 인덱스를 이용하여 품종 출력 (두번째 강아지)
-if predicted_breed2 in class_index_to_breed:
-    dog2 = class_index_to_breed[predicted_breed2]
-    print("Predicted Breed:", dog2)
-else:
-    print("Invalid breed index:", predicted_breed2)
-
-prompt = f"Show an image of a puppy born from a crossbreed between a {dog1} and a {dog2}."
+prompt = f"Show an image of a puppy born from a crossbreed between a {predicted_breed1} and a {predicted_breed2}."
 image_url = generate_image(prompt)
 save_image(image_url, "generated_puppy.png")
